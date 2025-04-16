9f9e6b3c42e4b519436eb1d522c8a7db63341525
Nik Panas <nikpanas@Niks-MacBook-Air.local>
2025-04-16 12:38:59 +1000

    Move index.html to public and confirm React mount

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,17 @@
-console.log('App running');
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Fix My Planet 🌏</h1>
+      <p>Stripe is working. React is mounted. Time to go green!</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
