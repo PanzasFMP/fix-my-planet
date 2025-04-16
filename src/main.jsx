779e038900e569dd985567b41040279fe49af47a
Nik Panas <nikpanas@Niks-MacBook-Air.local>
2025-04-16 12:43:03 +1000

    Restore App import to enable full React site

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-function App() {
-  return (
-    <div style={{ textAlign: 'center', padding: '2rem' }}>
-      <h1>Fix My Planet 🌏</h1>
-      <p>Stripe is working. React is mounted. Time to go green!</p>
-    </div>
-  );
-}
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
