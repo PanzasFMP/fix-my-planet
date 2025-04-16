a46160476c521f38953fffa8d3c5c477a19fc86d
Nik Panas <nikpanas@Niks-MacBook-Air.local>
2025-04-16 13:48:10 +1000

    Fix missing App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Fix My Planet 🌏</h1>
+      <p>This is the working base. Stripe and React are ready to go!</p>
+    </div>
+  );
+}
+
+export default App;
